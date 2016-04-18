f53054523abbd5024c3b5fe65ced268cb737121e
Ton Holsink <tonholsink@gmail.com>
2016-04-18 14:41:59 +0200

    small change

--- src/js/components/layout/Footer.js
+++ src/js/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 export default class Footer extends React.Component {
-    render() {
+    static render() {
         return (
             <footer>
                 <div class="row">
