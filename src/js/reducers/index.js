cfca56e2d67cf5839be2371b40eb89081d2618df
Ton Holsink <tonholsink@gmail.com>
2016-04-18 11:15:19 +0200

    small change

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,10 +1,6 @@
 import { combineReducers } from 'redux';
-import todos from './todos';
-import counter from './counter';
 import reddit from './reddit';
 
 export default combineReducers({
-    todos,
-    counter,
     reddit
 });
