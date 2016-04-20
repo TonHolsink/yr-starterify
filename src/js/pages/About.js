adfbeb17dc585ac8af8e6a6edc077edb0bf2137a
Administrator <admin@example.com>
2016-04-20 14:50:38 +0200

    haha ik lekker jou overschrijven

--- src/js/pages/About.js
+++ src/js/pages/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => (
     <div>
-        <h2>About22222</h2>
+        <h2>About222222242</h2>
         <p>React Starterify aims to give you a good starting point for your projects.</p>
         <p>If you're looking for a minimal ES6 (ES2015) React JS starter
             with nice shallow rendering test examples, this is probably for you.</p>
